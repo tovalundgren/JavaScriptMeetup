bc4c7921042a70d7838e14aa7bb60c2b54f3fcf8
David Heidari <davidheidari2@gmail.com>
2023-11-10 15:20:15 +0100

    Added som comment to my JS-kod

--- JS/info.js
+++ JS/info.js
@@ -1,6 +1,6 @@
 const mainContent = document.querySelector('.main-content');
 
-// Fetch
+// Fetchar 
 async function getInfo() {
     console.log("getInfo")
     const response = await fetch("../JSON/info.json");
@@ -16,17 +16,27 @@ async function getInfo() {
 // Function 
 function createInfo(infos) {
     infos.forEach(function (info) {
-            
+
+        // Skapar li-element som är förälder     
         const infoList = document.createElement('li');
+
+        // skapar h4-element inuti li-elementet 
         const infoListHeader = document.createElement('h4');
+
+        // skapar p-element inuti li-elementet 
         const infoListPara = document.createElement('p');
 
-        infoListHeader.textContent = `${info.list}:`; 
+        // Vi hämtar text-content från JSON-filen med key:list
+        infoListHeader.textContent = `${info.list}:`;
+        
+        // Vi hämtar text-content från JSON-filen med key:info
         infoListPara.textContent = info.info;  
 
+        // Appendar de variabel som vi har skapat 
         infoList.appendChild(infoListHeader);  
         infoList.appendChild(infoListPara); 
         console.log(info)
+
         mainContent.appendChild(infoList); 
     });    
 }
@@ -35,6 +45,3 @@ function createInfo(infos) {
 getInfo();
 
 
-function hej(){
-    console.log("hej");
-}
