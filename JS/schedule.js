bfbc94929e3587bdd9fb38c20ca963992b65c70f
Tova Lundgren <tova.lundgren@chasacademy.se>
2023-11-10 11:44:49 +0100

    added js code to schedule

--- JS/schedule.js
+++ JS/schedule.js
@@ -1,3 +1,5 @@
+const mainContent = document.getElementsByClassName("main-content"); //hämtar element från index.HTML
+
 async function getSchedule (){
     const response = await fetch("../JSON/schedule.json");
 
@@ -8,4 +10,19 @@ async function getSchedule (){
     const scheduleList = document.createElement("ul");
 }
 
+function showSchedule(json){
+    json.schedule.forEach(schedule => {
+        //skapa ett H3 element HTML:
+        const newH3 = document.createElement('h3');
+        //hämtar innehåll till H3 element från JSON:
+        newH3.textContent = schedule.event;
+        //döp klass för CSS
+        newH3.classList.add = 'schedule-header';
+        //h3 existeras, men är ej kopplad till HTML:
+        mainContent.appendChild(newH3);
+        console.log(newH3);
+    });
+}
+
 getSchedule();
+showSchedule();
